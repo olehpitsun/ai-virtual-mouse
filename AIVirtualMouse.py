afb8558700c222bfa5a6ad5041aa5045b2061947
Oleh Pitsun <oleh.pitsun@plumrocket.com>
2022-05-30 21:35:08 +0300

    add scrool

--- AIVirtualMouse.py
+++ AIVirtualMouse.py
@@ -3,6 +3,7 @@ import numpy as np
 import HandTrackingModule as htm
 import time
 import autopy
+import time
 
 ######################
 wCam, hCam = 640, 480
@@ -13,7 +14,7 @@ smoothening = 7  #random value
 pTime = 0
 plocX, plocY = 0, 0
 clocX, clocY = 0, 0
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
@@ -38,32 +39,27 @@ while True:
         cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
                       (255, 0, 255), 2)
 
-        # Step4: Only Index Finger: Moving Mode
-        if fingers[1] == 1 and fingers[2] == 0:
-
-            # Step5: Convert the coordinates
-            x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
-            y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
-
-            # Step6: Smooth Values
-            clocX = plocX + (x3 - plocX) / smoothening
-            clocY = plocY + (y3 - plocY) / smoothening
-
-            # Step7: Move Mouse
-            autopy.mouse.move(wScr - clocX, clocY)
-            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-            plocX, plocY = clocX, clocY
+      
 
         # Step8: Both Index and middle are up: Clicking Mode
-        if fingers[1] == 1 and fingers[2] == 1:
+        if fingers[0] == 1 and fingers[1] == 1:
 
             # Step9: Find distance between fingers
             length, img, lineInfo = detector.findDistance(8, 12, img)
 
             # Step10: Click mouse if distance short
             if length < 40:
-                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
-                autopy.mouse.click()
+                cv2.circle(img, (lineInfo[3], lineInfo[4]), 15, (0, 255, 0), cv2.FILLED)
+                #autopy.mouse.click()
+                #autopy.bitmap.capture_screen().save('/home/oleh/Зображення/screen.png')
+                #autopy.key.scroll(10)  
+                autopy.key.tap(autopy.key.Code.PAGE_DOWN)
+            if length > 40:
+                cv2.circle(img, (lineInfo[3], lineInfo[4]), 15, (0, 255, 0), cv2.FILLED)
+                #autopy.mouse.click()
+                #autopy.bitmap.capture_screen().save('/home/oleh/Зображення/screen.png')
+                #autopy.key.scroll(10)  
+                autopy.key.tap(autopy.key.Code.PAGE_UP)
 
     # Step11: Frame rate
     cTime = time.time()
